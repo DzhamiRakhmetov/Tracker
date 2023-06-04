eb5d6c8236b589b398e6562b04e133998eedb70c
Dzhami <rakhmetov.dzhami@yandex.ru>
2023-06-04 18:15:44 +0700

    CoreData init.

--- Services/TrackerCategoryStore.swift
+++ Services/TrackerCategoryStore.swift
@@ -17,27 +17,39 @@ final class TrackerCategoryStore: NSObject {
     private let context: NSManagedObjectContext
     weak var delegate: TrackerCategoryStoreDelegate?
     
-//    private lazy var fetchedResultsController: NSFetchedResultsController<TrackerCoreData> = {
-//        let fetchedRequest = TrackerCategoryCoreData.fetchRequest()
-//        fetchedRequest.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
-//        let controller = NSFetchedResultsController(
-//            fetchRequest: fetchedRequest,
-//            managedObjectContext: context,
-//            sectionNameKeyPath: nil,
-//            cacheName: nil)
-//
-//      //  controller.delegate = self
-//        try? controller.performFetch()
-//
-//        return controller
-//    }()
+    private lazy var fetchedResultsController: NSFetchedResultsController<TrackerCategoryCoreData> = {
+        let fetchedRequest = TrackerCategoryCoreData.fetchRequest()
+        fetchedRequest.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        let controller = NSFetchedResultsController(
+            fetchRequest: fetchedRequest,
+            managedObjectContext: context,
+            sectionNameKeyPath: nil,
+            cacheName: nil)
+
+        controller.delegate = self
+        try? controller.performFetch()
+
+        return controller
+    }()
     
     
     init(context: NSManagedObjectContext) {
         self.context = context
     }
     
-//    override convenience init() {
-//        
-//    }
+   override convenience init() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            fatalError("Error: appDelegate not found")
+        }
+        self.init(context: appDelegate.persistentContainer.viewContext)
+    }
+}
+
+// MARK: - NSFetchedResultsControllerDelegate
+
+extension TrackerCategoryStore: NSFetchedResultsControllerDelegate {
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        delegate?.didUpdateCategories()
+    }
 }
